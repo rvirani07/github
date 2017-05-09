6cb93611618eee06d215ef2938c3fb0c4aba9ac1
Rahul <rahulvirani32@gmail.com>
2017-05-08 21:19:19 -0400

    commit 2

--- src/Sample/Sample1.java
+++ src/Sample/Sample1.java
@@ -9,6 +9,9 @@ public class Sample1 {
 	public static void main(String[] args) {
 		System.out.println("Hello Rahul");
 		System.out.println("How are you ?");
+
+
+		System.out.println("Branch 1");
 	}
 
 }
