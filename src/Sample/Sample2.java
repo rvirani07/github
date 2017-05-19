c71ac47036b8d519242a6974eec843f74d4fe4e6
Rahul <rahulvirani32@gmail.com>
2017-05-18 20:09:33 -0400

    commit 4

---
+++ src/Sample/Sample2.java
@@ -0,0 +1,20 @@
+package Sample;
+
+/**
+ * Created by Rahul on 2017-05-18.
+ */
+public class Sample2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Rahul");
+        /*System.out.println("How are you ?");
+
+
+        System.out.println("Branch 1");
+
+        System.out.println("Branch 1 commit 2");*/
+
+
+
+    }
+}
